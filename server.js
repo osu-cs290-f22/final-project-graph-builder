426a87fb9f135c32f33dfcc18c1e08a0d828b807
KieranHostetlerMcLaughlin <92497265+KieranHostetlerMcLaughlin@users.noreply.github.com>
2022-11-29 14:55:17 -0800

    When you open the site, the first page you see (if you want no other page) is the homepage.html
    Changed homescreen.html to index.html

--- server.js
+++ server.js
@@ -1,21 +1,47 @@
-const express = require('express')
-const path = require('path')
-const Chart = require('chart')
-const html2canvas = require('html2canvas')
+//stuff we'll need of server stuff
+const express = require('express');
+const path = require('path');
+const handlebars = require('handlebars');
+const exphbs = require('express-handlebars');
+const fs = require('fs');
 
+//stuff we'll need speficially for this applicaton
+const Chart = require('chart');
+const html2canvas = require('html2canvas');
+
+
+//app is how we'll use express
 const app = express();
 
-app.get('/', function(req, res, next) {
-    res.status(200).sendFile(__dirname + "/public/initialGraph.html")
-})
 
-app.get('/initialGraphStyles.css', function(req, res) {
-    res.status(200).sendFile(__dirname + "/public/initialGraphStyles.css")
-})
+/*******************
+ * SETTING UP HANDLEBARS
+ **********************/
 
-app.get('/initialGraph.js', function(req, res) {
-    res.status(200).sendFile(__dirname + "/public/initialGraph.js")
-})
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+/***************
+ * HTTP REQUESTS
+ * Some of these are commented out.
+ **************/
+ 
+/*app.get('/', function(req, res, next) {res.status(200).sendFile(__dirname + "/public/initialGraph.html")})*/
+
+/*app.get('/', function(req, res, next) {res.status(200).render("index")});*/
+
+
+
+//get the home page
+
+app.get('/', function(req, res, next) {res.status(200).sendFile("/public/index.html")});
+
+
+app.post('/', function(req, res, next) {
+    //we will be using JSON
+});
 
 
 
